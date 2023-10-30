8804e3d2a1d11ff8431063b28f5058ffd49e6a08
Orr Avrech <orravrech@gmail.com>
2023-10-30 12:19:54 -0400

    first login steps

--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -1,19 +1,31 @@
-Given(/^User not logged in$/) do
-  visit '/logout'
-end
+# Given(/^I go to (.*) page/) do |page_name|
+#   visit path_to(page_name)
+# end
 
-Given(/^User logged in$/) do
-  visit '/mock_login'
-end
+# Then(/^I should be on the (.*) page/) do |page_name|
+#   expect(page).to have_current_path(path_to(page_name))
+# end
 
-Given(/^User logged in as (\d+)$/) do |uid|
-  visit "/mock_login/#{uid}"
-end
+# Then(/^I should see (.*)/) do |content|
+#   expect(page).to have_content(content)
+# end
 
-Given(/^User logged in with OAuth/) do
-  visit "/auth/google_oauth2/callback"
-end
+# Given(/^User not logged in$/) do
+#   visit '/logout'
+# end
 
-Then(/^I can see "([^"]*)" logged in$/) do |user_name|
-  step "I should see \"Welcome, #{user_name}\""
-end
+# Given(/^User logged in$/) do
+#   visit '/mock_login'
+# end
+
+# Given(/^User logged in as (\d+)$/) do |uid|
+#   visit "/mock_login/#{uid}"
+# end
+
+# Given(/^User logged in with OAuth/) do
+#   visit "/auth/google_oauth2/callback"
+# end
+
+# Then(/^I can see "([^"]*)" logged in$/) do |user_name|
+#   step "I should see \"Welcome, #{user_name}\""
+# end
