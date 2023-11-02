83bc181efa4cb33d44229b5ba4a032712199fd96
Kev <kevinxmedina@gmail.com>
2023-11-01 23:58:58 -0400

    changed default for course thing

--- db/migrate/20231101234712_create_courses.rb
+++ db/migrate/20231101234712_create_courses.rb
@@ -5,8 +5,8 @@ class CreateCourses < ActiveRecord::Migration
       t.string :course_number, null: false
       t.string :course_title
       t.string :offering_term, null: false
-      t.boolean :midterm, default: 0 # 0: no midterm, 1: midterm
-      t.boolean :project, default: 1 # 0: no project, 1: project
+      t.boolean :midterm, default: false # 0: no midterm, 1: midterm
+      t.boolean :project, default: true # 0: no project, 1: project
       t.string :instructor
       t.string :time
       t.string :days
